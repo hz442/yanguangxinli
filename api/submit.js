7fe0d4ab80a749cfdab5a5ceda5bd9f8c862d73e
hz442 <3549448559@qq.com>
2026-04-14 14:03:13 +0800

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,1 +1,9 @@
-
+export async function onRequestPost(context) {
+  const db = context.env.DB
+  const data = await context.request.json()
+  const id = Date.now().toString(16)
+  await db.prepare(`
+    INSERT INTO records (id,name,class,status,content) VALUES (?,?,?,?,?)
+  `).bind(id,data.name,data.class,data.status,data.content).run()
+  return Response.json({success:true,id})
+}
