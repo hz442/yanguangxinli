2d660e3c498fbc8bf3098dbb5d99f6b852d7d24b
hz442 <3549448559@qq.com>
2026-04-14 14:02:36 +0800

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,1 +1,9 @@
-
+export async function onRequestPost(context) {
+  const {user,pwd} = await context.request.json()
+  const adminUser = 'admin'       // 你可以修改
+  const adminPwd = '123456'       // 请修改为强密码
+  if(user===adminUser && pwd===adminPwd){
+    return Response.json({success:true,token:'admin-'+Date.now()})
+  }
+  return Response.json({success:false})
+}
