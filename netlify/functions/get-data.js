aa195fe5de37117256ce423d52edd7f5e7ece7e7
hz442 <3549448559@qq.com>
2026-04-14 11:27:22 +0800

    Update get-data.js

--- netlify/functions/get-data.js
+++ netlify/functions/get-data.js
@@ -1,1 +1,22 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+const DATA_FILE = path.join('/tmp', 'submissions.json');
+
+exports.handler = async () => {
+  try {
+    const file = await fs.readFile(DATA_FILE, 'utf8');
+    const data = JSON.parse(file);
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (e) {
+    // 文件不存在，返回空数组
+    return {
+      statusCode: 200,
+      body: JSON.stringify([])
+    };
+  }
+};
