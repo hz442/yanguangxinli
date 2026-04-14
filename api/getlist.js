1278d4cab9a3f2b38ff701c60d74c72ed4fd4ce4
hz442 <3549448559@qq.com>
2026-04-14 13:48:30 +0800

    Update getlist.js

--- api/getlist.js
+++ api/getlist.js
@@ -1,1 +1,11 @@
-
+export async function onRequestGet(context) {
+  const token = context.request.headers.get('token')
+  if(!token || !token.startsWith('admin-')){
+    return Response.json([])
+  }
+  const db = context.env.DB
+  const {results} = await db.prepare(`
+    SELECT * FROM records ORDER BY id DESC
+  `).all()
+  return Response.json(results)
+}
